4fda2e2a5533886bf4d446600eba5d2020595eca
Rui Pires <rpires.projects@gmail.com>
2020-12-01 15:22:34 +0000

    Day one challanges

--- src/main/kotlin/aoc2020/Day1.kt
+++ src/main/kotlin/aoc2020/Day1.kt
@@ -1,5 +1,68 @@
 package aoc2020
 
-class Day1 {
-    fun hello() = println("Hello")
+
+/**
+ *  List of numbers
+ *  Sum to X
+ *
+ *  Find pair which sums to a given value
+ *
+ *  [1 3 5 4 7] -> 10
+ *
+ *  Set -> [1, 3, 5, 4] : O(1)
+ *
+ */
+class ChallengeOne {
+
+    fun run(input: List<Int>, sum: Int): Pair<Int, Int> {
+        val found = mutableSetOf<Int>()
+
+        for (i in input) {
+            val complement = sum - i
+            if (found.contains(complement)) {
+                return Pair(complement, i)
+            }
+            found.add(i)
+        }
+        return Pair(0, 0)
+    }
+
+}
+
+
+/**
+ *
+ *  List of numbers
+ *  Sum to X
+ *
+ *  Find triple which sums to a given value
+ *
+ * i in [5, 4, 1, 2] -> 11
+ *  set = []
+ *  complement = sum - i
+ *  j in [4, 1, 2]
+ *      if (complement - arr[j]) in set
+ *          FOUND ALL
+ *      set.add(arr[j])
+ *
+ */
+class ChallengeTwo {
+
+    fun run(input: List<Int>, sum: Int): Int {
+
+        for(i in input.indices) {
+            val found = mutableSetOf<Int>()
+
+            val complement = sum - input[i]
+            for(j in i+1 until input.size) {
+                if (found.contains(complement - input[j])) {
+                    println("Triple is: ${input[i]}, ${input[j]}, ${complement - input[j]}")
+                    return input[i] * input[j] * (complement - input[j])
+                }
+                found.add(input[j])
+
+            }
+        }
+        return 0
+    }
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -1,16 +1,47 @@
 package test.kotlin
 
-import aoc2020.Day1
+import aoc2020.ChallengeOne
+import aoc2020.ChallengeTwo
+import helpers.Helpers
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day1Test {
 
     @Test
-    fun testDay1() {
-        val day1 = Day1()
-        day1.hello()
+    fun testChallengeOneFunction() {
 
+        val input = listOf(1721, 979, 366, 299, 675, 1456)
+
+        val (a, b) = ChallengeOne().run(input, 2020)
+
+        assertEquals(2020, a + b)
+        assertEquals(514579, a * b)
+    }
+
+    @Test
+    fun testChallengeOne() {
+        val input = Helpers.getFileResources("day1_luis.txt").map { it.toInt() }
+        val (a, b) = ChallengeOne().run(input, 2020)
+
+        println("Pair is $a, $b : ${a*b}")
         assert(true)
     }
 
+    @Test
+    fun testChallengeTwoFunction() {
+        val input = listOf(1721, 979, 366, 299, 675, 1456)
+
+        val mult = ChallengeTwo().run(input, 2020)
+
+        assertEquals(241861950, mult)
+    }
+
+    @Test
+    fun testChallengeTwo() {
+        val input = Helpers.getFileResources("day1_luis.txt").map { it.toInt() }
+        val mult = ChallengeTwo().run(input, 2020)
+        println(mult)
+        assert(true)
+    }
 }
